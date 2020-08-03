f918b8d8471fdb76bd95806899f70b3bce9fda95
EricChen <749829987@qq.com>
2020-08-04 00:09:06 +0800

    data

---
+++ 11-data-structures-algorithms/00-code/src/main/java/list/single/Example.java
@@ -0,0 +1,41 @@
+package list.single;
+
+public class Example {
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * 插入头节点
+     */
+    public static void insertHead(Node head, Node newNode){
+        Node old = head;
+        head = newNode;
+        head.next = old;
+    }
+
+    /**
+     * 插入尾节点
+     */
+    public static void insertTail(Node tail, Node newTail){
+        Node old = tail;
+        tail = newTail;
+        tail.next = null;
+        old.next = tail;
+    }
+
+    /**
+     * 遍历
+     */
+    public static void traverse(Node head){
+        while (head !=null){
+            System.out.println(head.value + "");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    
+
+}
---
+++ 11-data-structures-algorithms/00-code/src/main/java/list/single/Node.java
@@ -0,0 +1,14 @@
+package list.single;
+
+
+/**
+ * 单链表的 Node
+ */
+class Node {
+    int value;
+    Node next;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
